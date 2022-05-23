e1a12d2a828c978b742cf0abffb748d4675d02b5
Filippo Della Rupa <filippo.dellarupa@gmail.com>
2022-05-23 15:27:33 +0200

    Esercizi PHP selfwork ok

---
+++ index.php
@@ -0,0 +1,64 @@
+<?php
+
+// SCRIVERE UN PROGRAMMA CHE CALCOLI LA MEDIA DI UN ARRAY DI NUMERI INTERI, CONSIDERANDO SOLO I NUMERI DIVISIBILI PER 2
+
+// $numbers = [1,2,3,4,5,6,7,8,9,10];
+
+// $avarege = 0;
+
+// $counter = 0;
+
+
+// foreach ($numbers as $number) {
+//     if($number % 2==0){
+//         $avarege=$number + $avarege;
+//         $counter++;
+//     }
+// }
+
+// $final =$avarege/$counter;
+// echo ("$final");
+
+
+// Creare un array di persone con nome, cognome e genere. Se il genere e’ M stampare “Buongiorno Sig.” e se e’ F stampare “Buongiorno Sig.ra”
+
+// $people = [
+//     "name" => "Filippo",
+//     "surname" => "DR",
+//     "gender" => "F",
+// ];
+
+// if ($people["gender"]=="M") {
+//     echo "Buongiono Sig $people[name]";
+// } else {
+//     echo "Buongiono Sig.ra $people[name]";
+// }
+
+// Scrivere un codice che cacoli il BMI (indice di massa corporea) e che mi dia in console i risultati. BMI= peso in kg / statura in metri al quadrato
+
+// $heigth= 1.80;
+// $weigth= 100;
+
+// $BMI= $weigth/ ($heigth*$heigth);
+
+// echo "$BMI"
+
+
+
+// - Scrivere un programma che stampi in console tutti i numeri da uno a cento. Se il numero e’ multiplo di 3, non deve stampare il numero ma “PHP”; se multiplo di 5 deve stampare “JAVASCRIPT”; se multiplo di 3 e 5 (15) deve stampare “HACKADEMY43”;
+
+// for ($i=1; $i<=100 ; $i++) { 
+//     if($i%3 == 0 && $i%5 == 0){
+//         echo "HACKADEMY43 \n";
+//     } else if ($i%5 == 0){
+//         echo "JAVASCRIPT \n";
+//     } else if ($i%3 == 0){
+//         echo "PHP \n";
+//     } else {
+//         echo "$i \n";
+//     }
+// }
+
+
+
+?>
